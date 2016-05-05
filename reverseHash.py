0b22f66b74c45f67588100f2221da585fee15df6
admin <admin@haris-MacBook-Air.local>
2016-05-05 12:18:57 +0530

    updated reverHash

--- reverseHash.py
+++ reverseHash.py
@@ -4,7 +4,11 @@ str1 = 'acdegilmnoprstuw'
 str2=''
 for i in range(0,9):
 	temp=n%37
+	print temp
+	print "\n"
 	str2=str2+str1[temp];
 	n=n//37
+	if n == 7:
+		break;
 print(str2[::-1])
 #For 930846109532517 the string is lawnmower  
